917bb2b410d6eb153b50d6c265bac10a08c9e636
亮 <949126175@qq.com>
2018-07-09 19:46:23 +0800

    1

---
+++ Python07081/hello.py
@@ -0,0 +1,8 @@
+print("hello world")
+print ("hello agin")
+print ("I like typing this")
+print ("This is fun")
+
+print("请输入您的名字")
+name = input()
+print("my name is",name)
---
+++ PythonTest0709/test2.py
@@ -0,0 +1,72 @@
+print ("\n\n\n----------part one----------")
+#A comment(注释) ,this is so you can read 
+#your program later.
+#python中#号的用法
+#Anything after the # is ignored by python.
+print("I could have code like this.")#and the 
+#comment after is ignored
+#You can also use a comment to "disable" or 
+#comment out to a piece of code:
+#print "This won't run."
+print("This will run.")
+
+
+print ("\n\n\n----------part two----------")
+print ("I will now count my chickens")#我现在要数小鸡了
+print ("Hens",25.0+30.0/6.0)#母鸡30
+print ("Roosters",100.0-25.0*3.0%4.0)#公鸡97
+print ("Now I will count the eggs:")#我现在马上数鸡蛋
+print (3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0)#七个蛋//运算符与/运算符的区别要注意
+print ("Is it true that 3+2<5-7?")#这个表达式正确吗
+print (3.0+2.0<5.0-7.0)
+print ("What is 3+2?",3.0+2.0)#3加2等于多少
+print ("What is 5-7?",5.0-7.0)#5-7等于多少
+print ("Oh.that's why it's false.")#原来坐错了呢
+print ("How about some more.")#再多做写计算怎么样
+print ("Is it greater?",5.0>-2.0)#5大于-2吗
+print ("Is it greater or equal?",5.0>=-2.0)#5大于等于-2吗
+print ("Is it less or equal?",5.0<=-2.0)#5小于-2吗
+
+
+print ("\n\n\n----------part three----------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
